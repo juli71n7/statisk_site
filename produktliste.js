c699c3eb421c332800099d8e7d204fd3a247162c
Julie Gejel <juli71n7@stud.kea.dk>
2023-02-05 14:29:29 +0100

    fprsøg på produktliste

--- produktliste.js
+++ produktliste.js
@@ -1,4 +1,4 @@
-const fil = `https://kea-alt-del.dk/t7/api/products`;
+const fil = `https://kea-alt-del.dk/t7/api/products?limit=20`;
 const imageParth = `https://kea-alt-del.dk/t7/images/webp/640/.webp`;
 
 function hentData() {
@@ -7,7 +7,7 @@ function hentData() {
     .then(visData);
 }
 
-const temp = document.querySelector("#produktTemplate").content;
+const temp = document.querySelector("#smallProduktTemplate").content;
 const beholder = document.querySelector("main");
 
 function visData(data) {
@@ -15,13 +15,50 @@ function visData(data) {
     console.log(data);
 
     const klon = temp.cloneNode(true);
-    klon.querySelector("pic").src = imageParth;
-    klon.querySelector("name").textContent = produkt.productdisplayname;
-    klon.querySelector("brand").textContent = produkt.brandname;
-    klon.querySelector("price").textContent = produkt.price;
+    klon.querySelector("h3").textContent = produkt.productdisplayname;
+    klon.querySelector("#pic").src = imageParth;
+    klon.querySelector(".price").textContent = produkt.price;
+    klon.querySelector("#brand").textContent = produkt.brandname;
+    klon.querySelector("#type").textContent = produkt.articletype;
+    klon.querySelector("#category").textContent = produkt.category;
 
+    if (produkt.souldOut) {
+      klon.querySelector("article").classList.add("soldOut");
+    }
+
+    if (produkt.discounted) {
+      klon.querySelector("article").classList.add("onSale");
+    }
     beholder.appendChild(klon);
   });
 }
 
 hentData();
+
+/*
+articletype: "Tshirts"
+​​
+brandname: "Nike"
+​​
+category: "Apparel"
+​​
+discount: null
+​​
+gender: "Men"
+​​
+id: 1163
+​​
+price: 895
+​​
+productdisplayname: "Sahara Team India Fanwear Round Neck Jersey"
+​​
+productionyear: 2011
+​​
+season: "Summer"
+​​
+soldout: 0
+​​
+subcategory: "Topwear"
+​​
+usagetype: "Sports"
+*/
